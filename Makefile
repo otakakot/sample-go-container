e5e51eae5fcf9c10cf6ef34779bf3d71aa449b27
otakakot <117251387+otakakot@users.noreply.github.com>
2026-05-02 13:55:06 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+SHELL := /bin/bash
+export APP_NAME := $(basename $(notdir $(shell pwd)))
+
+.PHONY: help
+help: ## display this help screen
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-15s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+.PHONY: mod
+mod: ## go modules
+	@go get -u -t ./...
+	@go mod tidy
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"cmp"
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	port := cmp.Or(os.Getenv("PORT"), "8080")
+
+	env := cmp.Or(os.Getenv("ENV"), "local")
+
+	hdl := http.NewServeMux()
+
+	hdl.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		_, _ = w.Write([]byte("Hello, " + env + "!\n"))
+	})
+
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           hdl,
+		ReadHeaderTimeout: 30 * time.Second,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	defer stop()
+
+	go func() {
+		slog.Info("start server listen")
+
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			panic(err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	slog.Info("start server shutdown")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+
+	slog.Info("done server shutdown")
+}
